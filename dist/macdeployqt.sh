0b41de6e62d2ff0acdcea0a17d2f5582999c6584
Tim Baker <timnbaker@gmail.com>
2026-08-02 13:17:44 -0700

    Removed macdeployqt.sh. Changed make-dist-mac.sh to remove symlinks.

--- dist/macdeployqt.sh
+++
@@ -1,3 +0,0 @@
-cd ~/Programming/buildinged/Qt_6_11_1_for_macOS_Release/bin/
-~/Qt/6.11.1/macOS/bin/macdeployqt BuildingEd.app
-otool -L BuildingEd.app/Contents/MacOS/BuildingEd
--- dist/make-dist-mac.sh
+++ dist/make-dist-mac.sh
@@ -1,12 +1,23 @@
+#!/bin/bash
 SRC=$(pwd)/..
-BUILD=$SRC/../Qt_6_11_1_for_macOS_Release/bin
-DESTROOT=$SRC/../../ProjectZomboid
+BUILD=$(realpath $SRC/../Qt_6_11_1_for_macOS_Release/bin)
+DESTROOT=$(realpath $SRC/../../ProjectZomboid)
 DEST=$DESTROOT/BuildingEd
 
 mkdir -p $DESTROOT
 mkdir -p $DEST
 
+rm -rf $DEST/BuildingEd.app
 cp -a $BUILD/BuildingEd.app $DEST
+
+# Add all necessary Qt libraries
+~/Qt/6.11.1/macOS/bin/macdeployqt $DEST/BuildingEd.app
+
+# Remove all symlinks, they won't work on Steam
+find $DEST/BuildingEd.app/Contents -type l -delete
+
+mv $DEST/BuildingEd.app/Contents/Frameworks/libtiled.1.0.0.dylib $DEST/BuildingEd.app/Contents/Frameworks/libtiled.1.dylib
+mv $DEST/BuildingEd.app/Contents/Frameworks/libzlib1.1.0.0.dylib $DEST/BuildingEd.app/Contents/Frameworks/libzlib1.1.dylib
 
 cp -a $SRC/LICENSE.APACHE $DEST
 cp -a $SRC/LICENSE.BSD $DEST
