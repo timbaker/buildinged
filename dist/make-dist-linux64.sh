695335497b24995a1efbdda01fd1acbafc01426c
Tim Baker <timbaker@theindiestone.com>
2026-08-02 13:35:37 -0700

    Fixed make-dist-linux64.sh

--- dist/make-dist-linux64.sh
+++ dist/make-dist-linux64.sh
@@ -4,15 +4,15 @@ APPIMAGE=$SRC/../linuxdeploy-buildinged/BuildingEd-x86_64.AppImage
 DESTROOT=$SRC/../dist-tiled-docker
 DEST=$DESTROOT/BuildingEd
 
-mkdir $DESTROOT
-mkdir $DEST
+mkdir -p $DESTROOT
+mkdir -p $DEST
 cp -a $APPIMAGE $DEST
 cp -ar $BUILD/share/ $DEST
 
 cp -a $SRC/dist/BuildingEd-x86_64.AppImage.sh $DEST
 chmod +x $DEST/BuildingEd-x86_64.AppImage.sh
 
-cp -a $SRC/LICENSE.APACHE.txt $DEST
-cp -a $SRC/LICENSE.BSD.txt $DEST
-cp -a $SRC/LICENSE.GPL.txt $DEST
+cp -a $SRC/LICENSE.APACHE $DEST
+cp -a $SRC/LICENSE.BSD $DEST
+cp -a $SRC/LICENSE.GPL $DEST
 cp -a $SRC/LICENSE.QT6 $DEST
