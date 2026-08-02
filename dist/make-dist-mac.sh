c959a90124802835a84a57a6542ab17e35841541
Tim Baker <timnbaker@gmail.com>
2026-08-02 10:24:47 -0700

    Fixed make-dist-mac.sh

--- dist/make-dist-mac.sh
+++ dist/make-dist-mac.sh
@@ -1,15 +1,15 @@
 SRC=$(pwd)/..
-BUILD=$SRC/../Qt_6_11_1_for_macOS-Release/bin/
+BUILD=$SRC/../Qt_6_11_1_for_macOS_Release/bin
 DESTROOT=$SRC/../../ProjectZomboid
 DEST=$DESTROOT/BuildingEd
 
-mkdir $DESTROOT
-mkdir $DEST
+mkdir -p $DESTROOT
+mkdir -p $DEST
 
-cp -ra $BUILD/BuildingEd.app $DEST
+cp -a $BUILD/BuildingEd.app $DEST
 
-cp -a $SRC/LICENSE.APACHE.txt $DEST
-cp -a $SRC/LICENSE.BSD.txt $DEST
-cp -a $SRC/LICENSE.GPL.txt $DEST
+cp -a $SRC/LICENSE.APACHE $DEST
+cp -a $SRC/LICENSE.BSD $DEST
+cp -a $SRC/LICENSE.GPL $DEST
 cp -a $SRC/LICENSE.QT6 $DEST
 
